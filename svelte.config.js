6af1b746041f20eb93e69898ae42dceb2cac6efe
David <davidgonsal@gmail.com>
2025-05-05 15:48:49 +0200

    refactor(svelte.config.js): remove mdsvex from preprocessors for cleaner configuration ✂️

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -8,7 +7,7 @@ const basePath = process.env.BASE_PATH ?? '';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		adapter: adapter({
